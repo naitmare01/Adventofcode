3dfc4755bb17fd7d32e6f4f00f48b1c60aa91943
David Berndtsson <david.berndtsson@dataductus.se>
2020-12-07 15:25:02 +0100

    d6p2

--- 2020/day6/day6.py
+++ 2020/day6/day6.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import argparse
+from functools import reduce
 
 
 def arguments():
@@ -16,21 +17,30 @@ def arguments():
 class CustomCustoms():
     def __init__(self):
         self.answers = None
+        self.answers_pt2 = None
 
-    def find_anwers(self, group):
+    def find_anwers_pt1(self, group):
         self.answers = (len(set(group.replace("\n", ""))))
+
+def part2(input_path):
+    return sum(
+        [
+            len(reduce(lambda set_1, set_2: set_1.intersection(set_2), [set(word) for word in g if word])) for g in
+            [group.split('\n') for group in open(input_path).read().split('\n\n')]
+        ])
 
 
 def main():
     args = arguments()
     with open(args.file) as file:
+        print("Part2:", part2(args.file))
         input_file = file.read().strip().split("\n\n")
     result = []
     for row in input_file:
         custom_answers = CustomCustoms()
-        custom_answers.find_anwers(row)
+        custom_answers.find_anwers_pt1(row)
         result.append(custom_answers)
-    print(sum([x.answers for x in result]))
+    print("Part1:", sum([x.answers for x in result]))
 
 
 if __name__ == '__main__':
