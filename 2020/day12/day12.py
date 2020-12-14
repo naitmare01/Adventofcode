cc127802cec9e4a153dea712aa831275750f2a31
David Berndtsson <david.berndtsson@dataductus.se>
2020-12-14 11:31:58 +0100

    d12p2

--- 2020/day12/day12.py
+++ 2020/day12/day12.py
@@ -24,23 +24,56 @@ class Ship():
         self.position = {"x": 0, "y": 0}
         self.instructions = None
 
-    def run_ship(self):
-        for line in self.instructions:
-            instruction = line[0]
-            value = line[1]
+    def run_ship(self, part1):
+        if part1:
+            for line in self.instructions:
+                instruction = line[0]
+                value = line[1]
 
-            if instruction in self.directions:
-                self.position["x"] += (value * self.directions[instruction][0])
-                self.position["y"] += (value * self.directions[instruction][1])
+                if instruction in self.directions:
+                    self.position["x"] += value * self.directions[instruction][0]
+                    self.position["y"] += value * self.directions[instruction][1]
 
-            if instruction == "F":
-                facing = (self.angle_directions[self.angle])
-                self.position["x"] += (value * self.directions[facing][0])
-                self.position["y"] += (value * self.directions[facing][1])
+                if instruction == "F":
+                    facing = (self.angle_directions[self.angle])
+                    self.position["x"] += value * self.directions[facing][0]
+                    self.position["y"] += value * self.directions[facing][1]
 
-            if instruction in {"R", "L"}:
-                self.angle += (value if instruction == 'R' else 360 - value)
-                self.angle %= 360
+                if instruction in {"R", "L"}:
+                    self.angle += (value if instruction == 'R' else 360 - value)
+                    self.angle %= 360
+        else:
+            waypoint = Waypoint()
+            for line in self.instructions:
+                instruction = line[0]
+                value = line[1]
+
+                if instruction in self.directions:
+                    waypoint.position["x"] += value * self.directions[instruction][0]
+                    waypoint.position["y"] += value * self.directions[instruction][1]
+
+                if instruction == "F":
+                    self.position["x"] += value * waypoint.position["x"]
+                    self.position["y"] += value * waypoint.position["y"]
+
+                if instruction in {"R", "L"}:
+                    rotation = (value if instruction == 'R' else 360 - value)
+                    if rotation == 90 or rotation == 270:
+                        new_y_wp_position = waypoint.position["x"] * (-1 if rotation == 90 else 1)
+                        new_x_wp_position = waypoint.position["y"] * (1 if rotation == 90 else -1)
+                        waypoint.position["y"] = new_y_wp_position
+                        waypoint.position["x"] = new_x_wp_position
+                    elif rotation == 180:
+                        waypoint.position["y"] *= -1
+                        waypoint.position["x"] *= -1
+
+
+class Waypoint():
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.position = {"x": 10, "y": 1}
 
 
 def main():
@@ -51,8 +84,13 @@ def main():
         input_file = [(x[:1], int(x[1:])) for x in input_file]
     ship = Ship()
     ship.instructions = input_file
-    ship.run_ship()
+    ship.run_ship(True)
     print("Part1:", sum([abs(x) for x in ship.position.values()]))
+
+    ship.reset()
+    ship.instructions = input_file
+    ship.run_ship(False)
+    print("Part2:", sum([abs(x) for x in ship.position.values()]))
 
 
 if __name__ == '__main__':
