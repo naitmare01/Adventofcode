0768c25254ee6aafce9c51988d18d4d3987fd4d8
David Berndtsson <david.berndtsson@dataductus.se>
2020-12-25 21:02:07 +0100

    d24p2

--- 2020/day24/day24.py
+++ 2020/day24/day24.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import argparse
-# from functools import lru_cache
+from functools import lru_cache
 # from itertools import combinations
 # from copy import deepcopy
 import time
@@ -26,8 +26,9 @@ class LobbyLayout():
         self.directions = {'e': (1, -1, 0), 'se': (0, -1, 1), 'sw': (-1, 0, 1), 'w': (-1, 1, 0), 'nw': (0, 1, -1), 'ne': (1, 0, -1)}  # x,y,z
         self.reference_point = (0, 0, 0)
         self.tiles = dict()
+        self.black_tiles = set()
 
-    def parse_input(self):
+    def flip_tiles(self):
         for line in self.instructions:
             idx = 0
             tile = (0, 0, 0)
@@ -44,12 +45,42 @@ class LobbyLayout():
                 tile = tuple(map(sum, zip(add_cord, tile)))
 
             if self.tiles.get(tile):
-                if self.tiles[tile] == 'black':
+                if tile in self.black_tiles:
                     self.tiles[tile] = 'white'
+                    self.black_tiles.remove(tile)
                 else:
                     self.tiles[tile] = 'black'
+                    self.black_tiles.add(tile)
             else:
                 self.tiles[tile] = 'black'
+                self.black_tiles.add(tile)
+
+    def get_neighbors(self, tile):
+        return [tuple([tile[i] + d[i] for i in range(3)]) for d in [(1, -1, 0), (1, 0, -1), (0, 1, -1), (-1, 1, 0), (-1, 0, 1), (0, -1, 1)]]
+
+    @lru_cache(maxsize=None)
+    def conway_tiles(self):
+        for _ in range(100):
+            next_black = set()
+            for tile in self.black_tiles:
+                ns = self.get_neighbors(tile)
+                count_black = 0
+                for n in ns:
+                    if n in self.black_tiles:
+                        count_black += 1
+                if 0 < count_black <= 2:
+                    next_black.add(tile)
+                for n in ns:
+                    if n in self.black_tiles:
+                        continue
+                    n_of_ns = self.get_neighbors(n)
+                    count_black = 0
+                    for n_of_n in n_of_ns:
+                        if n_of_n in self.black_tiles:
+                            count_black += 1
+                    if count_black == 2:
+                        next_black.add(n)
+            self.black_tiles = {i for i in next_black}
 
 
 def main():
@@ -59,9 +90,12 @@ def main():
         input_file = file.read().splitlines()
     lobby = LobbyLayout()
     lobby.instructions = input_file
-    lobby.parse_input()
-    part1_result = len([v for k, v in lobby.tiles.items() if v == 'black'])
+    lobby.flip_tiles()
+    part1_result = len(lobby.black_tiles)
     print(f'Part1: {part1_result}')
+    lobby.conway_tiles()
+    part2_result = len(lobby.black_tiles)
+    print(f'Part2: {part2_result}')
     print(f'Execution time in seconds: {(time.time() - startTime)}')
 
 
