9e212b8bb0035e047d6fc70173803eed2df51aa3
David Berndtsson <david.berndtsson@gmail.com>
2023-01-02 14:03:54 +0100

    Rewrote logic

--- 2022/day09/day09.py
+++ 2022/day09/day09.py
@@ -19,6 +19,7 @@ class Snake:
     def __init__(self):
         self.head_pos = [0, 0]
         self.tail_pos = [0, 0]
+        self.knots = None
         self.visited = {(0, 0)}
         self.directions = {
             "R": [0, 1],
@@ -27,22 +28,31 @@ class Snake:
             "D": [-1, 0],
         }
         self.last_head_pos = None
+        self.last_leader_pos = None
+
+    def reset(self, num_knots):
+        self.knots = [[0, 0] for x in range(num_knots)]
 
     def travel(self, instruction):
         '''Travel'''
         direction, num_steps = instruction[0], int(instruction[1])
         for _ in range(num_steps):
             self.head_pos = [sum(x) for x in zip(self.head_pos, self.directions[direction])]
+            for idx, _ in enumerate(self.knots):
+                if idx == 0:
+                    leader = self.head_pos
+                else:
+                    leader = self.knots[idx - 1]
 
-            if self.head_pos != self.tail_pos:
-                adjacent_pos = list(self.adjac(self.tail_pos))
-                if len([x for x in adjacent_pos if x == self.head_pos]) == 0:
-                    if self.tail_pos[0] == self.head_pos[0] or self.tail_pos[1] == self.head_pos[1]:
-                        self.tail_pos = [sum(x) for x in zip(self.tail_pos, self.directions[direction])]
-                    else:
-                        self.tail_pos = self.last_head_pos # Diag move to last_head_pos
-            self.visited.add(tuple(self.tail_pos)) # Update visited tails location
-            self.last_head_pos = self.head_pos
+                if leader != self.knots[idx]:
+                    adjacent_pos = list(self.adjac(self.knots[idx]))
+                    if len([x for x in adjacent_pos if x == leader]) == 0:
+                        if self.knots[idx][0] == leader[0] or self.knots[idx][1] == leader[1]:
+                            self.knots[idx] = [sum(x) for x in zip(self.knots[idx], self.directions[direction])]
+                        else:
+                            self.knots[idx] = self.last_leader_pos # Diag move to last_head_pos
+                self.last_leader_pos = leader
+            self.visited.add(tuple(self.knots[-1]))
 
     def adjac(self, ele, sub = []):
         '''Adj'''
@@ -60,9 +70,15 @@ def main():
         input_file = input_file.splitlines()
         input_file = (x.split(" ") for x in input_file)
     snake = Snake()
-    for row in input_file:
-            snake.travel(row)
+    snake.reset(1)
+    snake_pt2 = Snake()
+    snake_pt2.reset(9)
+    for _, row in enumerate(input_file):
+        snake.travel(row)
+        snake_pt2.travel(row)
     print("Part1:", len(snake.visited))
+    print("Part2:", len(snake_pt2.visited))
+
 
 
 if __name__ == '__main__':
